5065b9731219f27ed4f5dee4d76090ec2b51aeaf
stefanusgusega <gunawanstefanus006@gmail.com>
2021-01-30 17:25:08 +0700

    debug exercise 5

--- w02_arduino_exercise/5_1_sketch.ino
+++ w02_arduino_exercise/5_1_sketch.ino
@@ -1,54 +1,58 @@
-/* ------------------------------------------------------------------------------- */
-// character LCD example code
-// www.hacktronics.com
+/*
+  LiquidCrystal Library - Hello World
 
+ Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
+ library works with all LCD displays that are compatible with the
+ Hitachi HD44780 driver. There are many of them out there, and you
+ can usually tell them by the 16-pin interface.
+
+ This sketch prints "Hello World!" to the LCD
+ and shows the time.
+
+  The circuit:
+ * LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
+ * LCD R/W pin to ground
+ * LCD VSS pin to ground
+ * LCD VCC pin to 5V
+ * 10K resistor:
+ * ends to +5V and ground
+ * wiper to LCD VO pin (pin 3)
+
+ Library originally added 18 Apr 2008
+ by David A. Mellis
+ library modified 5 Jul 2009
+ by Limor Fried (http://www.ladyada.net)
+ example added 9 Jul 2009
+ by Tom Igoe
+ modified 22 Nov 2010
+ by Tom Igoe
+
+ This example code is in the public domain.
+
+ http://www.arduino.cc/en/Tutorial/LiquidCrystal
+ */
+
+// include the library code:
 #include <LiquidCrystal.h>
 
-// Connections:
-// rs (LCD pin 4) to Arduino pin 12
-// rw (LCD pin 5) to Arduino pin 11
-// enable (LCD pin 6) to Arduino pin 10
-// LCD pin 15 to Arduino pin 13
-// LCD pins d4, d5, d6, d7 to Arduino pins 5, 4, 3, 2
-LiquidCrystal lcd(12, 11, 10, 5, 4, 3, 2);
-
-//byte smiley[8] = {
-  //B00000,
-  //B10001,
-  //B00000,
-  //B00000,
-  //B10001,
-  //B01110,
-  //B00000,
-//};
-
-int backLight = 13;    // pin 13 will control the backlight
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 const int kPinTemp = A0;
-void setup()
-{
-  pinMode(backLight, OUTPUT);
-  digitalWrite(backLight, HIGH); // turn backlight on. Replace 'HIGH' with 'LOW' to turn it off.
-  lcd.begin(16,2);              // columns, rows.  use 16,2 for a 16x2 LCD, etc.
-  lcd.clear();                  // start with a blank screen
-  
-    
-  lcd.setCursor(0,0);           // set cursor to column 0, row 0 (the first row)
-  // change text to whatever you like. keep it clean!
-  //lcd.setCursor(0,1);           // set cursor to column 0, row 1
-  //lcd.print("arduino lcd  ");
-  
-  //lcd.createChar(0, smiley);
-  //lcd.setCursor(15, 1);  
-  //lcd.write(byte(0));
-  
-  // inicializa la comunicación serial en 9600 bits por segundo
-  Serial.begin(9600);
-  
-  
-}
 
-void loop()
-{
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("Welcome!");
+}
+float maxF = -200;
+float minF = 200;
+void loop() {
   lcd.setCursor(0,0);           // set cursor to column 0, row 0 (the first row)
   lcd.print("Temp (C)");
   lcd.setCursor(0,1);
@@ -64,16 +68,15 @@ void loop()
   lcd.setCursor(0,0);           // set cursor to column 0, row 0 (the first row)
   lcd.print("Voltage");
   lcd.setCursor(0,1);
-  lcd.print(String(getMilliVolt())+" milliVolt");
+  lcd.print(String(getVolt())+" Volt");
   delay(1000);
   lcd.clear();
-
 }
 
-float getMilliVolt() {
+float getVolt() {
 	int reading = analogRead(kPinTemp);
   	float voltage = reading * 5.0 /1024;
-  	return voltage*1000;
+  	return voltage;
 }
 
 float getCelsius() {
